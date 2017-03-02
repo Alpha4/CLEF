814a2c7f2ad7972c41aaad0583c257f91bc7e34b
H4wak <theo.dolez@gmail.com>
2017-03-02 16:58:46 +0100

    Gui start

---
+++ src/extensions/ihm/IGUI.java
@@ -0,0 +1,23 @@
+package extensions.ihm;
+
+import framework.plugin.IMessage;
+
+public interface IGUI {
+	
+	
+	// fonctions utilisateurs
+	
+	
+	public void receiveMessage(IMessage message);
+	public void receiveRoomList();
+	public void connectedToServResponse();
+	
+	
+
+	// fonction host
+
+	
+	
+	
+
+}
