685b38b87510f394f679cd00b682bac4f77b1e5e
Esteban LAUNAY <E157554E@I005V2pc18.ensinfo.sciences.univ-nantes.prive>
2017-03-30 14:42:14 +0200

    Debut javadoc

--- src/framework/plugin/INetworkServer.java
+++ src/framework/plugin/INetworkServer.java
@@ -1,9 +1,14 @@
 package framework.plugin;
 
 import framework.IExtension;
-
+/**
+*@class Interface du serveur
+*@brief envoyer message au serveur
+*/
 public interface INetworkServer extends IExtension {
-
+/**
+*@brief methode appelée lors du lancement du serveur
+*/
 	void run();
 
 }
