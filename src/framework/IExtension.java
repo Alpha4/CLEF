92f117ebace37913e2a32c8a289563213cdac0e3
Camille LE LUET <E122442P@S046V7pc10.ensinfo.sciences.univ-nantes.prive>
2017-03-30 15:24:01 +0200

    Edition de la doc

--- src/framework/IExtension.java
+++ src/framework/IExtension.java
@@ -1,7 +1,10 @@
 package framework;
 
+/**
+ * Interface définissant les extensions
+ */
 public interface IExtension {
-	/*
+	/**
 	 * Méthode appelée au lancement de l'extension
 	 */
 	public void run();
--- src/framework/plugin/IGUI.java
+++ src/framework/plugin/IGUI.java
@@ -2,16 +2,19 @@ package framework.plugin;
 
 import framework.IExtension;
 
+/**
+ * Interface définissant l'interface graphique
+ */
 public interface IGUI extends IExtension{
 	
-	/*
+	/**
 	 * Reçoit le message
 	 * @param m : le message
 	 */
 	void receiveMessage(String m);
 	
 	
-	/*
+	/**
 	 * Initialisation
 	 */
 	public void initGUI();
