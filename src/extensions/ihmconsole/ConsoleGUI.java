54634aeb77753ec8c73328b35205c5f3aceabfe6
servantie <servantie.c@gmail.com>
2017-03-30 17:16:11 +0200

    debut d'ihm en console. reçoit les messages mais n'en envoie pas encore

--- src/extensions/ihmconsole/ConsoleGUI.java
+++ src/extensions/ihmconsole/ConsoleGUI.java
@@ -1,5 +1,8 @@
 package extensions.ihmconsole;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import framework.Framework;
 import framework.plugin.IGUI;
 import framework.plugin.INetworkClient;
@@ -18,15 +21,73 @@ public class ConsoleGUI implements IGUI{
 
 	private INetworkClient inetwork;
 	
+	public String getHostIP() {
+		return hostIP;
+	}
+
+	public void setHostIP(String hostIP) {
+		this.hostIP = hostIP;
+	}
+
+	public int getConnectionStatus() {
+		return connectionStatus;
+	}
+
+	public void setConnectionStatus(int connectionStatus) {
+		this.connectionStatus = connectionStatus;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public boolean isHost() {
+		return isHost;
+	}
+
+	public void setHost(boolean isHost) {
+		this.isHost = isHost;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public INetworkClient getInetwork() {
+		return inetwork;
+	}
+
+	public void setInetwork(INetworkClient inetwork) {
+		this.inetwork = inetwork;
+	}
+
+	public ConsoleGUI() {
+		inetwork = (INetworkClient) Framework.getExtension(INetworkClient.class);
+		hostIP = "localhost";
+		port = 1337;
+		pseudo = "Client";
+
+	}
+	
 	@Override
 	public void run() {
 		initGUI();
+		Framework.handleEvent("message.received", this);
 		
 	}
 
 	@Override
 	public void receiveMessage(String m) {
-		// TODO Auto-generated method stub
+		//comme les messages sont en pseudo : message
+		System.out.println(m);
 		
 	}
 	
@@ -35,15 +96,21 @@ public class ConsoleGUI implements IGUI{
 	@Override
 	public void initGUI() {
 		inetwork = (INetworkClient) Framework.getExtension(INetworkClient.class);
-		
+		inetwork.setPort(port);
+		inetwork.setServer(hostIP);
+		//on reçoit pseudo : message
 		System.out.println("Bienvenue sur CLEF, une application extensible");
+		System.out.println("Affichage en console du chat");
+		
+		
 		
 	}
 
 	@Override
 	public void handleEvent(String name, Object event) {
-		// TODO Auto-generated method stub
-		
+		if (name == "message.received") {
+			this.receiveMessage((String)event);
+		}
 	}
 
 }
