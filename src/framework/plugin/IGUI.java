7f38912de9d96a7bcfaaa295f67f894dd21f1e97
Camille LE LUET <E122442P@S046V7pc10.ensinfo.sciences.univ-nantes.prive>
2017-03-30 15:14:28 +0200

    Suppression des fichiers de test

--- src/framework/plugin/IGUI.java
+++ src/framework/plugin/IGUI.java
@@ -4,11 +4,30 @@ import framework.IExtension;
 
 public interface IGUI extends IExtension{
 	
-		
+	/*
+	 * Reçoit le message
+	 * @param m : le message
+	 */
 	void receiveMessage(String m);
+	
+	/*
+	 * Reçoit la list des rooms
+	 */
 	public void receiveRoomList();
+	
+	/*
+	 * Connexion au serveur
+	 */
 	public void connectedToServResponse();
+	
+	/*
+	 * Deconnexion du serveur
+	 */
 	public void disconnectedFromServer();
+	
+	/*
+	 * Initialisation
+	 */
 	public void initGUI();
 	
 	
